d2e3f594f0e3b349e06d21ab49e47ed4d08f198e
Dmitry Kolesov <kolesov.dm@gmail.com>
2015-03-25 09:15:33 +0300

    Normalization of data; Change parameters of the algorithm

--- src/connector.py
+++ src/connector.py
@@ -7,67 +7,88 @@ class SOM1d():
     '''
     def __init__(self, data):
         assert len(data.shape) == 2
-        self.data = data
-        
-        x_avg = np.average(data[:, 0], axis=0)
-        y_avg = np.average(data[:, 1], axis=0)
-        self.w = np.ones((data.shape[0]*2, 2))
-        self.w[:, 0] *= x_avg
-        self.w[:, 1] *= y_avg
-        
-#        idx = np.random.choice(range(self.data.shape[0]), self.data.shape[0])
-#        self.w = data[idx, :]
-#        self.w = data + np.random.normal(size=data.shape)
-        
-        
+        self.data = data.copy()
+
+        self.x_avg = np.average(data[:, 0], axis=0)
+        self.y_avg = np.average(data[:, 1], axis=0)
+
+        self.x_std = np.std(data[:, 0], axis=0)
+        self.y_std = np.std(data[:, 1], axis=0)
+
+        ratio = 4.0/3.0     # (Number of SOM unit) / (Number of points)
+        self.w = np.zeros((data.shape[0]*ratio, 2))
+
+
     @property
     def size(self):
         return self.w.shape[0]
+
+    def normalize(self):
+        self.data[:, 0] = (self.data[:, 0] - self.x_avg)/self.x_std
+        self.data[:, 1] = (self.data[:, 1] - self.y_avg)/self.y_std
+
+
+    def denormalyze(self):
+        self.data[:, 0] = self.x_std*self.data[:, 0] + self.x_avg
+        self.data[:, 1] = self.y_std*self.data[:, 1] + self.y_avg
+
+        self.w[:, 0] = self.x_std*self.w[:, 0] + self.x_avg
+        self.w[:, 1] = self.y_std*self.w[:, 1] + self.y_avg
 
     def distances(self, point):
         '''Return array of Euclidean distances between self.w and point
         '''
         diff = self.w - point
         return np.sqrt(np.sum(diff**2, axis=1))
-    
+
     def BMU_idx(self, point):
         '''Return index of best matching unit pf the point
         '''
         dists = self.distances(point)
         return np.argmin(dists)
-        
-    def gaussian(self, c, sigma):
+
+    def gaussian(self, c, sigma, circular=False):
         """ Returns a Gaussian centered in c """
         d = 2*np.pi * sigma**2
-        ax = np.exp(-np.power(range(self.size)-c, 2)/d)
-        return ax               
-        
-    def train(self,rlen, lrate_init=0.5, lrate_final=0.01, sigma_init=5.0, sigma_final=1):      
-#        import ipdb
-#        ipdb.set_trace()
-        for t in range(rlen):
-            
-            sigma = (sigma_final-sigma_init)*t/rlen + sigma_init
-            lrate = (lrate_final-lrate_init)*t/rlen + lrate_init
-        
-            point_id = np.random.choice(range(self.data.shape[0]), 1)
-            point = self.data[point_id, :]
+
+        if not circular:
+            dists = range(self.size)-c
+        else:
+            dx = np.abs(np.arange(self.size)-c)
+            dx1 = abs(self.size - np.mod(dx, self.size))
+            dists = np.min(np.array([dx, dx1]),  axis=0)
+
+        ax = np.exp(-np.power(dists, 2)/d)
+        return ax
+
+    def update(self, sigma, circular):
+        data = np.random.permutation(self.data)
+
+        for point in data:
             bmu = self.BMU_idx(point)
-        
+
             delta = (point - self.w[bmu, :])
-            bubble = self.gaussian(bmu, sigma)*lrate
-            
-            dx = delta[:, 0] * bubble
-            dy = delta[:, 1] * bubble
-            
+            bubble = self.gaussian(bmu, sigma, circular=circular)
+
+            dx = delta[0] * bubble
+            dy = delta[1] * bubble
+
             self.w[:, 0] += dx
             self.w[:, 1] += dy
-            
+
+    def train(self,rlen, lrate=0.99, sigma_init=5.0, circular=False):
+        sigma = sigma_init
+        for t in range(rlen):
+            sigma = sigma * lrate
+            self.update(sigma, circular)
+
     def connect(self):
         # train SOM
-        self.train(self.size*50, lrate_init=0.9, sigma_init=self.size, sigma_final=2)
-        self.train(self.size*500, lrate_init=0.9, sigma_init=2, sigma_final=0.1)
-        
+        self.normalize()
+        self.train(self.size*150, lrate=0.99, sigma_init=self.size, circular=False)
+        self.train(self.size*500, lrate=0.99, sigma_init=2, circular=False)
+        self.denormalyze()
+
         ordered = {}
         for point in self.data:
             bmu = self.BMU_idx(point)
@@ -75,9 +96,8 @@ class SOM1d():
                 ordered[bmu].append(point)
             except KeyError:
                 ordered[bmu] = [point]
-        
+
         result = []
-        print ordered
         for i in range(self.size):
             try:
                 pnts = ordered[i]
@@ -90,5 +110,30 @@ class SOM1d():
             except KeyError:
                 # It's Ok
                 pass
-                
+
         return result
+
+
+if __name__ == "__main__":
+    data = np.array([
+        [-0.1, 0],
+        [0, 1.1],
+        [0, 2.03],
+        [0, 3.2],
+        [0, 4.1],
+        [0, 5.2],
+        [0, 6.02],
+        [1, 5.1],
+        [1, 4.03],
+        [3, 1.5],
+        [2, 2.01],
+        [1, 2],
+        [1, 3]
+    ])
+
+    som = SOM1d(data)
+    print som.connect()
+
+    import matplotlib.pyplot as plt
+    plt.plot(som.w[:, 0], som.w[:, 1], 'r-o', som.data[:,0], som.data[:,1], 'o')
+    plt.show()
