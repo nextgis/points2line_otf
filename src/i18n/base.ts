d60c374b47c133a0858f3b3cbb77a96e369ed775
Maxim Dubinin <sim@gis-lab.info>
2015-04-06 13:11:08 +0300

    Update base.ts

--- src/i18n/base.ts
+++ src/i18n/base.ts
@@ -1,89 +1,89 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS><TS version="2.0">
 <context>
-    <name>Points2LineOTF</name>
+    <name>ReconstructLine</name>
     <message>
-        <location filename="points_2_line_otf.py" line="193"/>
-        <source>&amp;Points2Line OTF</source>
-        <translation type="unfinished"></translation>
+        <location filename="reconstruct_line.py" line="193"/>
+        <source>&amp;ReconstructLine</source>
+        <translation>&amp;ReconstructLine</translation>
     </message>
     <message>
-        <location filename="points_2_line_otf.py" line="166"/>
+        <location filename="reconstruct_line.py" line="166"/>
         <source>Get points</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="points_2_line_otf.py" line="172"/>
+        <location filename="reconstruct_line.py" line="172"/>
         <source>Insert line</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="points_2_line_otf.py" line="235"/>
+        <location filename="reconstruct_line.py" line="235"/>
         <source>Points was not copied</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="points_2_line_otf.py" line="225"/>
+        <location filename="reconstruct_line.py" line="225"/>
         <source>Select any vector layer and feature!</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="points_2_line_otf.py" line="230"/>
+        <location filename="reconstruct_line.py" line="230"/>
         <source>Only point vector layers are supported!</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="points_2_line_otf.py" line="235"/>
+        <location filename="reconstruct_line.py" line="235"/>
         <source>Select two or more points!</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="points_2_line_otf.py" line="314"/>
+        <location filename="reconstruct_line.py" line="314"/>
         <source>Points2Line OTF</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="points_2_line_otf.py" line="246"/>
+        <location filename="reconstruct_line.py" line="246"/>
         <source>Total points was copied: </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="points_2_line_otf.py" line="266"/>
+        <location filename="reconstruct_line.py" line="266"/>
         <source>Line can&apos;t be inserted</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="points_2_line_otf.py" line="255"/>
+        <location filename="reconstruct_line.py" line="255"/>
         <source>Select any vector layer and feature for inserting geom!</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="points_2_line_otf.py" line="259"/>
+        <location filename="reconstruct_line.py" line="259"/>
         <source>Only line vector layers are supported!</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="points_2_line_otf.py" line="263"/>
+        <location filename="reconstruct_line.py" line="263"/>
         <source>Layer is not editable!</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="points_2_line_otf.py" line="266"/>
+        <location filename="reconstruct_line.py" line="266"/>
         <source>Buffer is empty!</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="points_2_line_otf.py" line="271"/>
+        <location filename="reconstruct_line.py" line="271"/>
         <source>Processing points. Please wait...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="points_2_line_otf.py" line="309"/>
-        <source>One line was sucesfull added</source>
+        <location filename="reconstruct_line.py" line="309"/>
+        <source>One line was succesfull added</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="points_2_line_otf.py" line="314"/>
+        <location filename="reconstruct_line.py" line="314"/>
         <source>Line was not added</source>
         <translation type="unfinished"></translation>
     </message>
