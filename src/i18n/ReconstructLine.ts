87e765a7f24dc6a9bdd08e13465a870fa85d84e0
Maxim Dubinin <sim@gis-lab.info>
2015-04-06 13:07:12 +0300

    Update and rename Points2LineOTF_ru.ts to ReconstructLine.ts

--- src/i18n/ReconstructLine.ts
+++ src/i18n/ReconstructLine.ts
@@ -4,87 +4,87 @@
 <context>
     <name>Points2LineOTF</name>
     <message>
-        <location filename="points_2_line_otf.py" line="193"/>
+        <location filename="reconstruct_line.py" line="193"/>
         <source>&amp;Points2Line OTF</source>
         <translation>&amp;Points2Line OTF</translation>
     </message>
     <message>
-        <location filename="points_2_line_otf.py" line="166"/>
+        <location filename="reconstruct_line.py" line="166"/>
         <source>Get points</source>
         <translation>Копировать точки</translation>
     </message>
     <message>
-        <location filename="points_2_line_otf.py" line="172"/>
+        <location filename="reconstruct_line.py" line="172"/>
         <source>Insert line</source>
         <translation>Вставить линию</translation>
     </message>
     <message>
-        <location filename="points_2_line_otf.py" line="235"/>
+        <location filename="reconstruct_line.py" line="235"/>
         <source>Points was not copied</source>
         <translation>Точки не скопированы</translation>
     </message>
     <message>
-        <location filename="points_2_line_otf.py" line="225"/>
+        <location filename="reconstruct_line.py" line="225"/>
         <source>Select any vector layer and feature!</source>
         <translation>Выберете векторный слой и объекты!</translation>
     </message>
     <message>
-        <location filename="points_2_line_otf.py" line="230"/>
+        <location filename="reconstruct_line.py" line="230"/>
         <source>Only point vector layers are supported!</source>
         <translation>Поддерживаются только точечные слои! </translation>
     </message>
     <message>
-        <location filename="points_2_line_otf.py" line="235"/>
+        <location filename="reconstruct_line.py" line="235"/>
         <source>Select two or more points!</source>
         <translation>Выберите как минимум две точки!</translation>
     </message>
     <message>
-        <location filename="points_2_line_otf.py" line="314"/>
+        <location filename="reconstruct_line.py" line="314"/>
         <source>Points2Line OTF</source>
         <translation>Points2Line OTF</translation>
     </message>
     <message>
-        <location filename="points_2_line_otf.py" line="246"/>
+        <location filename="reconstruct_line.py" line="246"/>
         <source>Total points was copied: </source>
         <translation>Всего точек скопировано: </translation>
     </message>
     <message>
-        <location filename="points_2_line_otf.py" line="266"/>
+        <location filename="reconstruct_line.py" line="266"/>
         <source>Line can&apos;t be inserted</source>
         <translation>Линия не может быть вставлена</translation>
     </message>
     <message>
-        <location filename="points_2_line_otf.py" line="255"/>
+        <location filename="reconstruct_line.py" line="255"/>
         <source>Select any vector layer and feature for inserting geom!</source>
         <translation>Выберете векторный слой для вставки геометрии!</translation>
     </message>
     <message>
-        <location filename="points_2_line_otf.py" line="259"/>
+        <location filename="reconstruct_line.py" line="259"/>
         <source>Only line vector layers are supported!</source>
         <translation>Поддерживаются только линейные слои!</translation>
     </message>
     <message>
-        <location filename="points_2_line_otf.py" line="263"/>
+        <location filename="reconstruct_line.py" line="263"/>
         <source>Layer is not editable!</source>
         <translation>Слой не в режиме редактирования!</translation>
     </message>
     <message>
-        <location filename="points_2_line_otf.py" line="266"/>
+        <location filename="reconstruct_line.py" line="266"/>
         <source>Buffer is empty!</source>
         <translation>Буфер обмена пуст!</translation>
     </message>
     <message>
-        <location filename="points_2_line_otf.py" line="271"/>
+        <location filename="reconstruct_line.py" line="271"/>
         <source>Processing points. Please wait...</source>
         <translation>Обработка точек. Пожалуйста подождите...</translation>
     </message>
     <message>
-        <location filename="points_2_line_otf.py" line="309"/>
+        <location filename="reconstruct_line.py" line="309"/>
         <source>One line was sucesfull added</source>
         <translation>Одна линия успешно добавлена</translation>
     </message>
     <message>
-        <location filename="points_2_line_otf.py" line="314"/>
+        <location filename="reconstruct_line.py" line="314"/>
         <source>Line was not added</source>
         <translation>Линия не добавлена</translation>
     </message>
