a42e15b1eb2cb1b4b9b63cb0839de2bf7f21f72a
Eduard Kazakov <silenteddie@gmail.com>
2022-02-10 13:36:03 +0300

    return back license and readme, fix ru translation error, update metadata

---
+++ README.md
@@ -0,0 +1,17 @@
+# ReconstructLine
+QGIS tool to reconstruct linear features from points. Points can be unordered time-wise, only spatial location of points is needed and considered.
+
+QGIS hub page: http://plugins.qgis.org/plugins/reconstruct_line
+
+Report issues here: https://github.com/nextgis/ReconstructLine/issues
+
+Video demo: https://www.youtube.com/watch?v=qDM3bZNAf7Y
+
+Manuals:
+
+* RU: http://gis-lab.info/qa/reconstruct_line.html
+* EN: https://translate.google.com/translate?sl=ru&tl=en&js=y&prev=_t&hl=ru&ie=UTF-8&u=http%3A%2F%2Fgis-lab.info%2Fqa%2Freconstruct_line.html&edit-text=
+
+License
+-------------
+This program is licensed under GNU GPL v2 or any later version
--- i18n/ReconstructLine_ru.ts
+++ i18n/ReconstructLine_ru.ts
@@ -3,77 +3,77 @@
 <context>
     <name>ReconstructLine</name>
     <message>
-        <location filename="reconstruct_line.py" line="167"/>
+        <location filename="reconstruct_line.py" line="171"/>
         <source>Get points</source>
         <translation>Копировать точки</translation>
     </message>
     <message>
-        <location filename="reconstruct_line.py" line="173"/>
+        <location filename="reconstruct_line.py" line="177"/>
         <source>Insert line</source>
         <translation>Вставить линию</translation>
     </message>
     <message>
-        <location filename="reconstruct_line.py" line="357"/>
+        <location filename="reconstruct_line.py" line="356"/>
         <source>ReconstructLine</source>
         <translation>ReconstructLine</translation>
     </message>
     <message>
-        <location filename="reconstruct_line.py" line="282"/>
+        <location filename="reconstruct_line.py" line="285"/>
         <source>Processing points. Please wait...</source>
         <translation>Обработка точек. Пожалуйста подождите...</translation>
     </message>
     <message>
-        <location filename="reconstruct_line.py" line="357"/>
+        <location filename="reconstruct_line.py" line="361"/>
         <source>Line was not added</source>
         <translation>Линия не добавлена</translation>
     </message>
     <message>
-        <location filename="reconstruct_line.py" line="67"/>
+        <location filename="reconstruct_line.py" line="69"/>
         <source>&amp;Reconstruct line</source>
         <translation>&amp;Reconstruct line</translation>
     </message>
     <message>
-        <location filename="reconstruct_line.py" line="193"/>
+        <location filename="reconstruct_line.py" line="196"/>
         <source>&amp;Reconstruct Line</source>
         <translation>&amp;Reconstruct Line</translation>
     </message>
     <message>
-        <location filename="reconstruct_line.py" line="228"/>
+        <location filename="reconstruct_line.py" line="231"/>
         <source>No points! Choose point layer and select points!</source>
         <translation>Точки не скопированы! Выберите точечный слой и несколько точек в нем!</translation>
     </message>
     <message>
-        <location filename="reconstruct_line.py" line="236"/>
+        <location filename="reconstruct_line.py" line="239"/>
         <source>No points! Select two or more points in the layer!</source>
         <translation>Точки не скопированы! Выберите две или более точек в слое!</translation>
     </message>
     <message>
-        <location filename="reconstruct_line.py" line="246"/>
+        <location filename="reconstruct_line.py" line="249"/>
         <source>Total points was copied: %d. Use &apos;Insert line&apos; button on new or existing line layer to make a line.</source>
         <translation>Всего скопировано точек: %d. Используйте кнопку &apos;Вставить линию&apos; для нового или существующего линейного слоя.</translation>
     </message>
     <message>
-        <location filename="reconstruct_line.py" line="260"/>
+        <location filename="reconstruct_line.py" line="263"/>
         <source>Line can&apos;t be inserted! Select lines layer for inserting new geom!</source>
         <translation>Линия не может быть вставлена! Выберите линейный слой для вставки новой геометрии!</translation>
     </message>
     <message>
-        <location filename="reconstruct_line.py" line="267"/>
+        <location filename="reconstruct_line.py" line="270"/>
         <source>Line can&apos;t be inserted! Layer is not editable!</source>
         <translation>Линия не может быть вставлена! Слой не в режиме редактирования!</translation>
     </message>
     <message>
-        <location filename="reconstruct_line.py" line="274"/>
+        <location filename="reconstruct_line.py" line="277"/>
         <source>Line can&apos;t be inserted! Copy points first!</source>
         <translation>Линия не может быть вставлена! Сначала скопируйте точки!</translation>
     </message>
     <message>
-        <location filename="reconstruct_line.py" line="179"/>
+        <location filename="reconstruct_line.py" line="183"/>
         <source>Insert multiple lines</source>
         <translation>Вставить составную линию</translation>
     </message>
     <message>
-        <location filename="reconstruct_line.py" line="353"/>
+        <location filename="reconstruct_line.py" line="357"/>
         <source>%s line segment(s) was sucesfull added</source>
         <translation>%s сегмент(ов) линии было успешно добавлено</translation>
     </message>
