076208efb95b0430685dc275d9cf158ed49ef8ee
Ivan Barsukov <i.barsukov@protonmail.com>
2024-11-20 21:48:09 +0700

    Update translations

--- src/reconstruct_line/i18n/ReconstructLine_ru.ts
+++ src/reconstruct_line/i18n/ReconstructLine_ru.ts
@@ -2,130 +2,191 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="ru_RU">
 <context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../about_dialog.py" line="220"/>
+        <source>Developers</source>
+        <translation>Разработчики</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="221"/>
+        <source>Homepage</source>
+        <translation>Домашняя страница</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="222"/>
+        <source>Please report bugs at</source>
+        <translation>Пожалуйста, сообщайте об ошибках в</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="225"/>
+        <source>Video with an overview of the plugin</source>
+        <translation>Видео с обзором плагина</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="226"/>
+        <source>Other helpful services by NextGIS</source>
+        <translation>Другие полезные сервисы от NextGIS</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="227"/>
+        <source>Convenient up-to-date data extracts for any place in the world</source>
+        <translation>Удобная выборка актуальных данных из любой точки мира</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="230"/>
+        <source>Fully featured Web GIS service</source>
+        <translation>Полнофункциональный Веб ГИС-сервис</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="215"/>
+        <source>REPORT_END</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="224"/>
+        <source>bugtracker</source>
+        <translation>багтрекер</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="75"/>
+        <source> by NextGIS</source>
+        <translation> от NextGIS</translation>
+    </message>
+</context>
+<context>
     <name>AboutDialogBase</name>
     <message>
-        <location filename="../about_dialog_base.ui" line="14"/>
+        <location filename="../ui/about_dialog_base.ui" line="14"/>
         <source>About {plugin_name}</source>
-        <translation>О {plugin_name}</translation>
+        <translation>О модуле {plugin_name}</translation>
     </message>
     <message>
-        <location filename="../about_dialog_base.ui" line="27"/>
-        <source>&lt;p align=&quot;center&quot;&gt;{plugin_name}&lt;/p&gt;</source>
-        <translation>&lt;p align=&quot;center&quot;&gt;{plugin_name}&lt;/p&gt;</translation>
+        <location filename="../ui/about_dialog_base.ui" line="33"/>
+        <source>Information</source>
+        <translation>Информация</translation>
     </message>
     <message>
-        <location filename="../about_dialog_base.ui" line="40"/>
-        <source>&lt;p&gt;{description}&lt;/p&gt;
-&lt;p&gt;{about}&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Developers:&lt;/b&gt; &lt;a href=&quot;{main_url}&quot;&gt;{authors}&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Homepage:&lt;/b&gt; &lt;a href=&quot;{homepage_url}&quot;&gt;{homepage_url}&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Video with an overview of the plugin:&lt;/b&gt; &lt;a href=&quot;{video_url}&quot;&gt;
-{video_url}&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Please report bugs at&lt;/b&gt; &lt;a href=&quot;{tracker_url}&quot;&gt;bugtracker&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;Other helpful services by NextGIS:
-&lt;ul&gt;&lt;li&gt;&lt;b&gt;Convenient up-to-date data extracts for any place in the world:
-&lt;a href=&quot;{main_url}&quot;&gt;{main_url}&lt;/a&gt;&lt;/b&gt;&lt;/li&gt;
-&lt;li&gt;&lt;b&gt;Fully featured Web GIS service:
-&lt;a href=&quot;{main_url}/nextgis-com/plans&quot;&gt;
-{main_url}/nextgis-com/plans&lt;/a&gt;&lt;/b&gt;&lt;/li&gt;&lt;/ul&gt;
-&lt;/p&gt;</source>
-        <translation>&lt;p&gt;{description}&lt;/p&gt;
-&lt;p&gt;{about}&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Разработчики:&lt;/b&gt; &lt;a href=&quot;{main_url}&quot;&gt;{authors}&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Домашняя страница:&lt;/b&gt; &lt;a href=&quot;{homepage_url}&quot;&gt;{homepage_url}&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Видео с обзором плагина:&lt;/b&gt; &lt;a href=&quot;{video_url}&quot;&gt;
-{video_url}&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Пожалуйста, сообщайте о багах в&lt;/b&gt; &lt;a href=&quot;{tracker_url}&quot;&gt;багтрекер&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;Другие полезные сервисы NextGIS:
-&lt;ul&gt;&lt;li&gt;&lt;b&gt;Удобная выборка актуальных данных из любой точки мира:
-&lt;a href=&quot;{main_url}&quot;&gt;{main_url}&lt;/a&gt;&lt;/b&gt;&lt;/li&gt;
-&lt;li&gt;&lt;b&gt;Полнофункциональный веб-ГИС-сервис:
-&lt;a href=&quot;{main_url}/nextgis-com/plans&quot;&gt;
-{main_url}/nextgis-com/plans&lt;/a&gt;&lt;/b&gt;&lt;/li&gt;&lt;/ul&gt;
-&lt;/p&gt;</translation>
+        <location filename="../ui/about_dialog_base.ui" line="72"/>
+        <source>License</source>
+        <translation>Лицензия</translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="94"/>
+        <source>Components</source>
+        <translation>Компоненты</translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="116"/>
+        <source>Contributors</source>
+        <translation>Участники</translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="158"/>
+        <source>{plugin_name}</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="168"/>
+        <source>Version {version}</source>
+        <translation>Версия {version}</translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="203"/>
+        <source>Get involved</source>
+        <translation>Присоединяйтесь</translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="54"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation></translation>
     </message>
 </context>
 <context>
     <name>ReconstructLine</name>
     <message>
-        <location filename="../reconstruct_line.py" line="170"/>
+        <location filename="../reconstruct_line.py" line="176"/>
         <source>Get points</source>
         <translation>Копировать точки</translation>
     </message>
     <message>
-        <location filename="../reconstruct_line.py" line="176"/>
+        <location filename="../reconstruct_line.py" line="183"/>
         <source>Insert line</source>
         <translation>Вставить линию</translation>
     </message>
     <message>
-        <location filename="../reconstruct_line.py" line="371"/>
+        <location filename="../reconstruct_line.py" line="435"/>
         <source>ReconstructLine</source>
         <translation>ReconstructLine</translation>
     </message>
     <message>
-        <location filename="../reconstruct_line.py" line="295"/>
+        <location filename="../reconstruct_line.py" line="343"/>
         <source>Processing points. Please wait...</source>
         <translation>Обработка точек. Пожалуйста подождите...</translation>
     </message>
     <message>
-        <location filename="../reconstruct_line.py" line="371"/>
+        <location filename="../reconstruct_line.py" line="435"/>
         <source>Line was not added</source>
         <translation>Линия не добавлена</translation>
     </message>
     <message>
-        <location filename="../reconstruct_line.py" line="70"/>
+        <location filename="../reconstruct_line.py" line="79"/>
         <source>&amp;Reconstruct line</source>
         <translation>&amp;Reconstruct line</translation>
     </message>
     <message>
-        <location filename="../reconstruct_line.py" line="202"/>
+        <location filename="../reconstruct_line.py" line="220"/>
         <source>&amp;Reconstruct Line</source>
         <translation>&amp;Reconstruct Line</translation>
     </message>
     <message>
-        <location filename="../reconstruct_line.py" line="241"/>
+        <location filename="../reconstruct_line.py" line="266"/>
         <source>No points! Choose point layer and select points!</source>
         <translation>Точки не скопированы! Выберите точечный слой и несколько точек в нем!</translation>
     </message>
     <message>
-        <location filename="../reconstruct_line.py" line="249"/>
+        <location filename="../reconstruct_line.py" line="276"/>
         <source>No points! Select two or more points in the layer!</source>
         <translation>Точки не скопированы! Выберите две или более точек в слое!</translation>
     </message>
     <message>
-        <location filename="../reconstruct_line.py" line="259"/>
+        <location filename="../reconstruct_line.py" line="291"/>
         <source>Total points was copied: %d. Use &apos;Insert line&apos; button on new or existing line layer to make a line.</source>
         <translation>Всего скопировано точек: %d. Используйте кнопку &apos;Вставить линию&apos; для нового или существующего линейного слоя.</translation>
     </message>
     <message>
-        <location filename="../reconstruct_line.py" line="273"/>
+        <location filename="../reconstruct_line.py" line="313"/>
         <source>Line can&apos;t be inserted! Select lines layer for inserting new geom!</source>
         <translation>Линия не может быть вставлена! Выберите линейный слой для вставки новой геометрии!</translation>
     </message>
     <message>
-        <location filename="../reconstruct_line.py" line="280"/>
+        <location filename="../reconstruct_line.py" line="324"/>
         <source>Line can&apos;t be inserted! Layer is not editable!</source>
         <translation>Линия не может быть вставлена! Слой не в режиме редактирования!</translation>
     </message>
     <message>
-        <location filename="../reconstruct_line.py" line="287"/>
+        <location filename="../reconstruct_line.py" line="333"/>
         <source>Line can&apos;t be inserted! Copy points first!</source>
         <translation>Линия не может быть вставлена! Сначала скопируйте точки!</translation>
     </message>
     <message>
-        <location filename="../reconstruct_line.py" line="182"/>
+        <location filename="../reconstruct_line.py" line="190"/>
         <source>Insert multiple lines</source>
         <translation>Вставить составную линию</translation>
     </message>
     <message>
-        <location filename="../reconstruct_line.py" line="367"/>
+        <location filename="../reconstruct_line.py" line="426"/>
         <source>%s line segment(s) was sucesfull added</source>
         <translation>%s сегмент(ов) линии было успешно добавлено</translation>
     </message>
     <message>
-        <location filename="../reconstruct_line.py" line="188"/>
+        <location filename="../reconstruct_line.py" line="197"/>
         <source>About</source>
-        <translation>О модуле...</translation>
+        <translation>О модуле</translation>
     </message>
 </context>
 </TS>
